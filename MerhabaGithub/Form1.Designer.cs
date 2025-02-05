4e33f197d8c93f52f6786d0d4c79ed836870adc1
Sinan URUN <sinanurun24@gmail.com>
2025-02-05 10:46:27 +0300

    Add project files.

---
+++ MerhabaGithub/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MerhabaGithub
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F);
+            label1.Location = new Point(98, 161);
+            label1.Name = "label1";
+            label1.Size = new Size(372, 65);
+            label1.TabIndex = 0;
+            label1.Text = "Merhaba Github";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.github;
+            pictureBox1.Location = new Point(189, 18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(184, 140);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(553, 271);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ MerhabaGithub/Form1.cs
@@ -0,0 +1,10 @@
+namespace MerhabaGithub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
